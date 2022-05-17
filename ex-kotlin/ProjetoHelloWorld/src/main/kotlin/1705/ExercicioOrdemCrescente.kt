31e2add0b4bb8f88455f28dd59426653d03e5dd1
Mayara Bispo <mayarareginabisp@gmail.com>
2022-05-17 13:08:46 -0300

    ExercicioOrdemCrescente

--- ex-kotlin/ProjetoHelloWorld/src/main/kotlin/1705/ExercicioOrdemCrescente.kt
+++ ex-kotlin/ProjetoHelloWorld/src/main/kotlin/1705/ExercicioOrdemCrescente.kt
@@ -15,7 +15,9 @@ fun main() {
     var b = 0
     var c = 0
 
-    if ((num1 < num2) && (num1 < num3)) { // caso o menor seja o num1
+    if ((num1 < num2) && (num1 < num3)) {
+
+        // caso o menor seja o num1
         a = num1
 
         if (num2 < num3) {
@@ -26,7 +28,9 @@ fun main() {
             c = num2
         }
 
-    } else if ((num2 < num3) && (num2 < num1)) { // caso o menor seja o num2
+    } else if ((num2 < num3) && (num2 < num1)) {
+
+        // caso o menor seja o num2
         a = num2
 
         if (num1 < num3) {
@@ -37,7 +41,9 @@ fun main() {
             c = num1
         }
 
-    } else if ((num3 < num1) && (num3 < num2)) { // caso o menor seja o num3
+    } else if ((num3 < num1) && (num3 < num2)) {
+
+        // caso o menor seja o num3
         a = num3
 
         if (num2 < num1) {
@@ -48,13 +54,14 @@ fun main() {
             c = num2
         }
 
-    } else{ // caso haja numeros iguais (num1 = num2, num2 = num3 ou num3 = num1)
+    } else{
 
+        // caso haja numeros iguais (num1 = num2, num2 = num3 ou num3 = num1)
         if((num1 == num2) && num1 < num3){
             a = num1
             b = num2
             c = num3
-        } else if(num2 == num3 && num2 < num1){
+        } else if((num2 == num3) && num2 < num1){
             a = num2
             b = num3
             c = num1
