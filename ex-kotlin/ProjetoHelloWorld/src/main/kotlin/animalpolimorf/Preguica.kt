b3e9d1da3e18da02bfef23515c79642ec39e8398
Mayara Bispo <mayarareginabisp@gmail.com>
2022-05-25 16:38:45 -0300

    25/05 - ExercicioAnimalPolimorfismo

--- ex-kotlin/ProjetoHelloWorld/src/main/kotlin/animalpolimorf/Preguica.kt
+++ ex-kotlin/ProjetoHelloWorld/src/main/kotlin/animalpolimorf/Preguica.kt
@@ -7,6 +7,9 @@ class Preguica(nome: String, idade: Int) :AnimalPoli(nome, idade) {
         println("..........")
     }
 
+    // Não interpretei subir árvores como um tipo de deslocamento (do modo que o Henrique colocou
+    // na resolução do exercicio), entendi que subir árvores é um método exclusivo da classe Preguica,
+    // já correr() é um método da classe pai Animal, que é herdado por Cachorro e Cavalo
     fun subirArvores(){
         println("Subindo árvores como uma preguiça.")
     }
